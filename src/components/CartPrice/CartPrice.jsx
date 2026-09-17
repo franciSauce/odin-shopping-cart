7cccf3f6c6ebb3e4ddd585752adb4e697de3e977
franciSauce <careyfrancisowino@gmail.com>
2026-09-17 10:21:08 +0300

    chore:setup imports, locs & export

---
+++ src/components/CartPrice/CartPrice.jsx
@@ -0,0 +1,22 @@
+import { mdiDelete } from '@mdi/js';
+import Icon from '@mdi/react';
+import styles from './Cart.module.css';
+
+const CartPrice = ({product, deleteProduct}) => {
+    return (
+        <div className={styles.box}>
+            <p>{product.title}</p>
+            <div>
+                <button onClick={deleteProduct}>
+                    <Icon>
+                        size={1}
+                        path={mdiDelete}
+                    </Icon>
+                </button>
+                <p>{(product.price * product.quantity).toFixed(2)} Ksh</p>
+            </div>
+        </div>
+    )
+}
+
+export default CartPrice;
